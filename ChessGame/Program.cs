041ee22d4ea3ccc7b2b534b7809487bde1c4eca3
Victor Destefani <a18586@alunos.ipca.pt>
2024-08-26 22:09:53 +0100

    classe posicao inserida

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace ChessGame
+﻿//  Arquitetura do sistema de xadrez:
+//  Camada de Aplicação         - Modo que consome os dados do utilizador 
+//  <-> Camada Jogo de Xadrez   - Regras do jogo
+//  <-> Camada Tabuleiro        - Representação dos objetos(peças, tabuleiro)
+// Em camadas facilita o projeto em manutenções, boas práticas de código pois temos tudo bem organizado e distinguido.
+using tabuleiro;
+
+namespace ChessGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao p; //objeto instanciado para posicao linha x coluna
+            p = new Posicao(3, 4);
+
+            Console.Write(p);
         }
     }
 }
